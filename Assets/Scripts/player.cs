63795828631d4b0b196ad3510f3737d08db51518
greendolf <ghost_4452@bk.ru>
2022-05-17 00:58:54 +0700

    movement rework 2, health system tryings

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    public int extJumps;
-    public int t_extJumps;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    public KeyCode jump = KeyCode.Space;
+
+
+    public float speed = 15;
+    public float jumpForce = 25;
+    private int extJumps;
+    public int extJumpsValue = 1;
 
     private Rigidbody2D rb;
 
@@ -15,39 +21,54 @@ public class player : MonoBehaviour
 
     private bool isGrounded;
     public Transform feetPos;
-    public float checkRadius;
+    public float checkRadius = 0.2f;
     public LayerMask whatIsGround;
 
+    public int hpValue = 100;
+    public int hp;
     void Start()
     {
-        t_extJumps = extJumps;
+        hp = hpValue;
+        extJumps = extJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
+        LifeLogic();
         MovementLogic();
         JumpLogic();
     }
 
-    private void MovementLogic()
+    //**************************************************************************************************
+
+    private void LifeLogic()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        rb.AddForce(movement * speed);
-        
-        if (Mathf.Abs(rb.velocity.x) > speed)
-        {
-            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * speed, rb.velocity.y);
-        }
-
-        if(facingRight == false && moveHorizontal > 0)
+        if (hp <= 0)
         {
             Flip();
         }
-        else if(facingRight == true && moveHorizontal < 0)
+    }
+    private void MovementLogic()
+    {
+        if (Input.GetKey(right))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(left))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (!facingRight && Input.GetKey(right))
+        {
+            Flip();
+        }
+        else if(facingRight && Input.GetKey(left))
         {
             Flip();
         }
@@ -56,30 +77,47 @@ public class player : MonoBehaviour
     private void JumpLogic()
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        if (Input.GetAxis("Jump") > 0)
+        /*if (Physics2D.Linecast(transform.position, feetPos.position, 3 << LayerMask.NameToLayer("Ground")))
         {
-            if (isGrounded)
-            {
-                rb.AddForce(Vector2.up * jumpForce);
-                extJumps = t_extJumps;
-            }
-            else if (extJumps > 0)
-            {
-                rb.AddForce(Vector2.up * jumpForce);
-                extJumps--;
-            }
+            isGrounded = true;
+        }*/
+        if (isGrounded)
+        {
+            extJumps = extJumpsValue;
         }
-    }
-    private void Update()
-    {
-        
+        if (Input.GetKeyDown(jump) && !isGrounded && (extJumps > 0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            extJumps--;
+        }
+        else if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     void Flip()
     {
         facingRight = !facingRight;
-        Vector3 Scaler = transform.localScale;
+        Vector2 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
+    }
+//**************************************************************************************************
+
+    /*void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject obj = collider.gameObject;
+        if (obj.CompareTag("Enemy"))
+        {
+            int dmg = obj.GetComponent<MeleeDamage>().dmg;
+            GetDamage(dmg);
+        }
+    }*/
+
+    public void GetDamage(int value)
+    {
+        print("Damaged");
+        hp -= value;
     }
 }
